cfa4b5e360e7c19d03b8c0b04b588c44d836a1a0
Seonghun An <53287605+shAn-kor@users.noreply.github.com>
2025-02-10 17:26:25 +0900

    Shan (#35)
    * Docs: Git Ignore Fix
    git ignore 파일 수정
    
    * Feat: Dog Photo List
    강아지 선택 시 특정 강아지 사진 목록 조회
    다시 선택 시 선택 취소
    
    * Feat: Add Photo,Badge Tab Menu
    강아지 선택 시 탭 메뉴 출력
    
    * Feat: Add Test Login
    로그인 검증 없는 테스트용 로그인 페이지
    
    * Feat: Add Chat
    채팅 기능 추가
    
    * Fix: Fix Chat List Content
    채팅방 이름 변경

--- src/components/Chat/ChatList.jsx
+++ src/components/Chat/ChatList.jsx
@@ -4,10 +4,10 @@ import "./ChatList.css"
 function ChatList({className}) {
 
   const chatroomList = [
-    {image: "/src/assets/img/user/ex_user_profile_01.png", name: "시바견 노예", message: "산책 ㄱㄱ?", date: "오늘", chatRoomNo: 1},
-    {image: "/src/assets/img/user/ex_user_profile_02.png", name: "웰시코기123", message: "웰시코기 귀여웡", date: "어제", chatRoomNo: 2},
-    {image: "/src/assets/img/user/ex_user_profile_03.png", name: "브라운박사", message: "견종이 어떻게 되시오 낭자~", date: "24.12.16", chatRoomNo: 3},
-    {image: "/src/assets/img/user/ex_user_profile_04.png", name: "도토리무침", message: "강아지 너무너무 귀여워요!!!! 진짜 또 보고싶어요ㅠㅠㅠㅠ 진짜진짜 귀여워요ㅠㅠㅠㅠㅠㅠㅠ", date: "24.12.15", chatRoomNo: 4},
+    {image: "/src/assets/img/user/ex_user_profile_01.png", name: "순찌부찌", message: "루비도 신날 거 같아요! 내일 봬요! 🐶💛", date: "오늘", chatRoomNo: 1},
+    {image: "/src/assets/img/user/ex_user_profile_02.png", name: "산책중독", message: "웰시코기 귀여웡", date: "어제", chatRoomNo: 2},
+    {image: "/src/assets/img/user/ex_user_profile_03.png", name: "간식좀", message: "견종이 어떻게 되시오 낭자~", date: "24.12.16", chatRoomNo: 3},
+    {image: "/src/assets/img/user/ex_user_profile_04.png", name: "호날두", message: "강아지 너무너무 귀여워요!!!! 진짜 또 보고싶어요ㅠㅠㅠㅠ 진짜진짜 귀여워요ㅠㅠㅠㅠㅠㅠㅠ", date: "24.12.15", chatRoomNo: 4},
   ];
 
   return(
